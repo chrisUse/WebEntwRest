af33d9942e11c7780c089786288927c9ffa0867d
Christian Linde <linde.chris@gmail.com>
2013-01-16 13:45:08 +0100

    add Product web service

--- src/java/resource/WebEntwProduct.java
+++ src/java/resource/WebEntwProduct.java
@@ -4,6 +4,7 @@
  */
 package resource;
 
+import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -21,5 +22,10 @@ public class WebEntwProduct {
     @WebMethod(operationName = "hello")
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
+    }
+    
+    @WebMethod(operationName = "products")
+    public List<data.Product> products() {
+        return data.Storage.getInstance().getAllProducts();
     }
 }
